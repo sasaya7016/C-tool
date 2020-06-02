07d72571e3d7302308caf880a04fa3e12fc9a6b0
sasaya7016 <sasaya7016@outlook.jp>
2020-06-02 23:46:03 +0900

    Create StorePostForm.php

---
+++ app/Http/Requests/StorePostForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'post_date' => 'required|date',
+            'title'     => 'required|text|max:255',
+            'image'     => 'required|text',
+            'keyword1'     => 'required|text',
+            'keyword2'     => 'required|text',
+            'content'      => 'required|text|max:10000'
+            
+        ];
+    }
+}
